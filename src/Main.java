95e6cc454b977d50208c4e79f193f413a71b217e
selinbsa <selinbasakagan@gmail.com>
2025-03-07 13:20:00 +0300

    loop düzenleme

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("Hoşgeldiniz! Artık yılı hesaplamak için lütfen yılı girimiz: ");
         int leap_year = scanner.nextInt();
 
-        if (leap_year%4 == 0 ||(leap_year % 100 != 0 && leap_year%400 ==0)){
+        if ((leap_year%4 == 0 && leap_year % 100 != 0) ||leap_year%400 ==0){
             System.out.println(leap_year + " bir artık yıldır ! ");
         }
         else {
