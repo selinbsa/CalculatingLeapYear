11372c8c72a31599366d777044eb38cbc9ad99d8
selinbsa <selinbasakagan@gmail.com>
2025-03-08 02:54:00 +0300

    Adding comments

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Taking year information from user
         System.out.println("Hoşgeldiniz! Artık yılı hesaplamak için lütfen yılı girimiz: ");
         int leap_year = scanner.nextInt();
 
+        //Checking is the year leap or not
         if ((leap_year%4 == 0 && leap_year % 100 != 0) ||leap_year%400 ==0){
             System.out.println(leap_year + " bir artık yıldır ! ");
         }
